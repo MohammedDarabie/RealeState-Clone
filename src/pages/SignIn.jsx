96c269eaae1cc15f9acb4d9808f317448399e5bb
MohdDarabie darabie <darabiemohammad@gmail.com>
2023-07-07 16:48:51 +0300

    Creating Sign In Functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../Components/OAuth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
 // import { FcGoogle } from "react-icons/fc";
 
 const SignIn = () => {
+  const navigate = useNavigate();
   const [show, setShow] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
@@ -23,14 +26,38 @@ const SignIn = () => {
     });
     console.log(formData);
   };
+  // ONSUBMIT FUNCTION
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredentials.user) {
+        navigate("/");
+        toast.success("Successfully Logged In");
+      } else {
+        toast.error("User not Found");
+      }
+    } catch (error) {
+      toast.error("Bad User Credentials");
+    }
+  }
 
+  // RETURN STATEMENT
   return (
     <section>
       <h1 className=" text-center mt-6 text-3xl font-bold font-mono ">
         Sign In
       </h1>
       {/* Div for Both */}
-      <div className="flex justify-around flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+      <div
+        className="flex justify-around flex-wrap items-center px-6
+       py-12 max-w-6xl mx-auto"
+      >
         {/* Div for img */}
         <div className="md:w-[67%] lg:w-[50%] s-w[100%] mb-12 md:mb-6  ">
           {/* Image */}
@@ -42,10 +69,11 @@ const SignIn = () => {
         </div>
         {/* Div For Form */}
         <div className=" w-full md:w-[67%] lg:w-[40%]">
-          <form className="">
+          <form onSubmit={onSubmit}>
             {/* Input Email */}
             <input
-              className="w-full px-4 py-2 text-xl mb-2 text-gray-700 bg-white border-gray-300 rounded-md
+              className="w-full px-4 py-2 text-xl mb-2 text-gray-700
+               bg-white border-gray-300 rounded-md
               transition ease-in-out"
               placeholder="Email address"
               type="email"
@@ -68,7 +96,10 @@ const SignIn = () => {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423
+                     7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963
+                      7.178.07.207.07.431 0 .639C20.577 16.49 16.64
+                       19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
                   />
                   <path
                     strokeLinecap="round"
@@ -78,7 +109,8 @@ const SignIn = () => {
                 </svg>
               ) : (
                 <svg
-                  className="w-6 h-6 absolute end-0 right-3 top-2 sm:text-sm cursor-pointer"
+                  className="w-6 h-6 absolute end-0 right-3 top-2
+                   sm:text-sm cursor-pointer"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -89,13 +121,19 @@ const SignIn = () => {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226
+                     16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228
+                      6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065
+                       7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228
+                        3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0
+                         0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
                   />
                 </svg>
               )}
 
               <input
-                className="w-full px-4 py-2 text-xl text-gray-700  bg-white border-gray-300 rounded-md
+                className="w-full px-4 py-2 text-xl text-gray-700
+                  bg-white border-gray-300 rounded-md
               transition ease-in-out"
                 placeholder="Password"
                 type={show ? "text" : "password"}
@@ -104,18 +142,23 @@ const SignIn = () => {
                 onChange={onChange}
               ></input>
             </div>
-            <div className="flex justify-between mt-5 whitespace-nowrap text-sm sm:text-lg">
+            <div
+              className="flex justify-between mt-5 whitespace-nowrap
+             text-sm sm:text-lg"
+            >
               <p className="flex">
                 Don't have an account?{" "}
                 <Link
-                  className="text-red-500 hover:text-red-700 transition duration-300 ease-in-out ml-1"
+                  className="text-red-500 hover:text-red-700
+                   transition duration-300 ease-in-out ml-1"
                   to={"/sign-up"}
                 >
                   Register
                 </Link>
               </p>
               <Link
-                className="text-blue-500  hover:text-blue-700 transition duration-300 ease-in-out"
+                className="text-blue-500  hover:text-blue-700
+                 transition duration-300 ease-in-out"
                 to={"/forgot-password"}
               >
                 Forgot password?
@@ -123,15 +166,20 @@ const SignIn = () => {
             </div>
             <button
               type="submit"
-              className=" bg-blue-600 text-white w-full px-7 py-3 rounded-md mt-3 text-sm font-medium uppercase shadow-md
-             hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg active:bg-blue-800"
+              className=" bg-blue-600 text-white w-full px-7 py-3
+               rounded-md mt-3 text-sm font-medium uppercase shadow-md
+             hover:bg-blue-700 transition duration-200 ease-in-out
+              hover:shadow-lg active:bg-blue-800"
             >
               SIGN IN
             </button>
             <div>
               <p
-                className=" relative before:absolute before:bg-black  before:h-[0.5px] before:w-[45%] before:top-[50%] before:left-0 before:sm-w-[40%]
-            after:absolute after:bg-black  after:h-[0.5px] after:w-[45%] after:top-[50%] after:right-0 after:sm-w-[40%]
+                className=" relative before:absolute before:bg-black
+                  before:h-[0.5px] before:w-[45%] before:top-[50%] before:left-0
+                   before:sm-w-[40%]
+            after:absolute after:bg-black  after:h-[0.5px] after:w-[45%]
+             after:top-[50%] after:right-0 after:sm-w-[40%]
             text-center mt-2 w-full font-semibold"
               >
                 OR
