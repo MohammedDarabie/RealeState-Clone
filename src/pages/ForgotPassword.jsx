a8e26f93e3f64f831bdf4cb968a8fa4f5ae07ca5
MohdDarabie darabie <darabiemohammad@gmail.com>
2023-07-07 17:09:33 +0300

    Forgot Password Functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import OAuth from "../Components/OAuth";
+import { toast } from "react-toastify";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 const ForgotPassword = () => {
-
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const onChange = (e) => {
     setEmail(e.target.value);
   };
+
+  // OnSubmit Function
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Successfully Sent");
+      
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
 
   return (
     <section>
@@ -15,22 +29,27 @@ const ForgotPassword = () => {
         Forgot Password
       </h1>
       {/* Div for Both */}
-      <div className="flex justify-around flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+      <div
+        className="flex justify-around flex-wrap items-center
+       px-6 py-12 max-w-6xl mx-auto"
+      >
         {/* Div for img */}
         <div className="md:w-[67%] lg:w-[50%] s-w[100%] mb-12 md:mb-6  ">
           {/* Image */}
           <img
-            className="w-full rounded-2xl border-1 border-black-600 drop-shadow-lg"
+            className="w-full rounded-2xl border-1 border-black-600
+             drop-shadow-lg"
             src="./img/sign-in-pic.jpg"
             alt="hand holding Building"
           ></img>
         </div>
         {/* Div For Form */}
         <div className=" w-full md:w-[67%] lg:w-[40%]">
-          <form className="">
+          <form onSubmit={onSubmit}>
             {/* Input Email */}
             <input
-              className="w-full px-4 py-2 text-xl mb-2 text-gray-700 bg-white border-gray-300 rounded-md
+              className="w-full px-4 py-2 text-xl mb-2 text-gray-700
+               bg-white border-gray-300 rounded-md
               transition ease-in-out"
               placeholder="Email address"
               type="email"
@@ -38,18 +57,23 @@ const ForgotPassword = () => {
               value={email}
               onChange={onChange}
             ></input>
-            <div className="flex justify-between mt-5 whitespace-nowrap text-sm sm:text-lg">
+            <div
+              className="flex justify-between mt-5 whitespace-nowrap
+             text-sm sm:text-lg"
+            >
               <p className="flex">
                 Don't have an account?
                 <Link
-                  className="text-red-500 hover:text-red-700 transition duration-300 ease-in-out ml-1"
+                  className="text-red-500 hover:text-red-700 transition
+                   duration-300 ease-in-out ml-1"
                   to={"/sign-up"}
                 >
                   Register
                 </Link>
               </p>
               <Link
-                className="text-blue-500  hover:text-blue-700 transition duration-300 ease-in-out"
+                className="text-blue-500  hover:text-blue-700 transition
+                 duration-300 ease-in-out"
                 to={"/sign-in"}
               >
                 Sign in
@@ -57,21 +81,26 @@ const ForgotPassword = () => {
             </div>
             <button
               type="submit"
-              className=" bg-blue-600 text-white w-full px-7 py-3 rounded-md mt-3 text-sm font-medium uppercase shadow-md
-             hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg active:bg-blue-800"
+              className=" bg-blue-600 text-white w-full px-7 py-3 rounded-md
+               mt-3 text-sm font-medium uppercase shadow-md
+             hover:bg-blue-700 transition duration-200 ease-in-out
+              hover:shadow-lg active:bg-blue-800"
             >
               SEND RESET EMAIL
             </button>
             <div>
               <p
-                className=" relative before:absolute before:bg-black  before:h-[0.5px] before:w-[45%] before:top-[50%] before:left-0 before:sm-w-[40%]
-            after:absolute after:bg-black  after:h-[0.5px] after:w-[45%] after:top-[50%] after:right-0 after:sm-w-[40%]
+                className=" relative before:absolute before:bg-black
+                  before:h-[0.5px] before:w-[45%] before:top-[50%] before:left-0
+                   before:sm-w-[40%]
+            after:absolute after:bg-black  after:h-[0.5px] after:w-[45%]
+             after:top-[50%] after:right-0 after:sm-w-[40%]
             text-center mt-2 w-full font-semibold"
               >
                 OR
               </p>
             </div>
-          <OAuth />
+            <OAuth />
           </form>
         </div>
       </div>
