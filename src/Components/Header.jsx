62fd82a156ca90a6cae1ca771cd78f28eb08dfe8
MohdDarabie darabie <darabiemohammad@gmail.com>
2023-07-08 07:02:13 +0300

    Making Header Dynamic

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
+
 const Header = () => {
   // Taking the Object from useLocation that contains Pathname under 'location.pathname'
   const location = useLocation();
   // Navigate is used to navigate through our website
   const navigate = useNavigate();
+  const auth = getAuth();
+  useEffect(()=>{
+    onAuthStateChanged(auth  , (user) => {
+      if(user){
+        setLoggedIn("Profile")
+      }else {
+        setLoggedIn("Sign In")
+      }
+    })
+  },[auth])
   // Function that return true if the path passed is equal to the location.pathname
-  function pathMathRoute(route) {
+  function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true;
     }
   }
-
+  const [loggedIn, setLoggedIn] = useState("SIGN IN");
   // Return Statement
   return (
     <div className="shadow-sm bg-white border-b sticky top-0 z-50">
       {/* Header Component */}
-      <header  className=" flex justify-between items-center px-3    max-w-6xl mx-auto">
+      <header className=" flex justify-between items-center px-3    max-w-6xl mx-auto">
         {/* Div Containing Logo */}
         <div
           //  OnCLick Command to navigate to requested EndPoint
@@ -38,7 +50,7 @@ const Header = () => {
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-700 border-b-[3px]
                border-b-transparent 
-               ${pathMathRoute("/") && "text-black border-b-blue-500 "}`}
+               ${pathMatchRoute("/") && "text-black border-b-red-500 "}`}
               //  OnCLick Command to navigate to requested EndPoint
               onClick={() => navigate("/")}
             >
@@ -48,7 +60,7 @@ const Header = () => {
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-700 border-b-[3px]
                border-b-transparent 
-                ${pathMathRoute("/offers") && "text-black border-b-blue-500"}`}
+                ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`}
               onClick={() => navigate("/offers")}
             >
               Offers
@@ -58,10 +70,13 @@ const Header = () => {
               className={`cursor-pointer py-3 text-sm font-semibold
                text-gray-700 border-b-[3px]
                border-b-transparent 
-               ${pathMathRoute("/sign-in") && "text-black border-b-blue-500"}`}
-              onClick={() => navigate("/sign-in")}
+               ${
+                 (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+                 "text-black border-b-red-500"
+               }`}
+              onClick={() => navigate("/profile")}
             >
-              Sign In
+              {loggedIn}
             </li>
           </ul>
         </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,8 @@
-import { getAuth, updateCurrentUser, updateProfile } from "firebase/auth";
-
+import { getAuth, updateProfile } from "firebase/auth";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase";
 
 const Profile = () => {
