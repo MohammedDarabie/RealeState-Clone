7166cebfedc2377326b693c6e9ad5b61cadef84f
MohdDarabie darabie <darabiemohammad@gmail.com>
2023-07-10 17:01:42 +0300

    Creating Slider in Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+
+import Slider from "../Components/Slider";
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  return (
+    <div>
+      <Slider />
+    </div>
+  );
+};
+
+export default Home;
